b439e2adbe83c2f709012469b75c715f46748b0a
Nate Stott <nate.stott@pm.me>
2024-03-23 08:21:10 -0600

    finish week2 problem1

--- src/Week2/Problem1/Solution1.java
+++ src/Week2/Problem1/Solution1.java
@@ -1,4 +1,68 @@
 package Week2.Problem1;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result1 {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyInteger(List<Integer> a) {
+    // Write your code here
+        Collections.sort(a);
+        List<Integer> b = new ArrayList<>();
+        for (int i = a.size()-1; i >= 0; i--) {
+            b.add(a.get(i));
+        }
+        int k = -1;
+        boolean found = false;
+        for (int i = 0; i < b.size()-1; i++) {
+            if (!Objects.equals(b.get(i), b.get(i + 1))) {
+                k = b.get(i);
+                found = true;
+                break;
+            }
+            else {
+                i++;
+            }
+        }
+        if (found) return k;
+        return b.get(b.size()-1);
+    }
+
+}
+
 public class Solution1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result1.lonelyInteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
