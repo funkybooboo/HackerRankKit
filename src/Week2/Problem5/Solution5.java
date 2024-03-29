4cfdfc5f2966c6725a66dcd3abd51b1dd332dce6
Nate Stott <nate.stott@pm.me>
2024-03-29 13:34:12 -0600

    finished week2 prob 5

---
+++ src/Week2/Problem5/Solution5.java
@@ -0,0 +1,63 @@
+package Week2.Problem5;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result5 {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+
+        List<Integer> frequency = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            frequency.add(0);
+        }
+        for (int a : arr) {
+            int count = frequency.get(a);
+            frequency.set(a, count+1);
+        }
+        return frequency;
+
+    }
+
+}
+
+public class Solution5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result5.countingSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
